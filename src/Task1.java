907410e14385fb75fab465d08220b6ddd850cd5d
Herman <german1514u@gmail.com>
2024-10-29 19:03:30 +0100

    add testB()

--- src/Task1.java
+++ src/Task1.java
@@ -5,4 +5,7 @@ public class Task1 {
     public static void testA(){
         System.out.println("Hello World!");
     }
+    public static void testB(){
+        System.out.println("Test B");
+    }
 }
